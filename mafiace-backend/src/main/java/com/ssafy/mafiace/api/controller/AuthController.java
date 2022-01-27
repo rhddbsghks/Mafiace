e304e8a169aeff3e2d49f5473b20ee347113c427
John <john@example.com>
2022-01-27 19:12:09 +0900

    [S06P12A602-41] fix: 첫 백 프론트 화합 v.2

--- mafiace-backend/src/main/java/com/ssafy/mafiace/api/controller/AuthController.java
+++ mafiace-backend/src/main/java/com/ssafy/mafiace/api/controller/AuthController.java
@@ -14,11 +14,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @Api(value = "인증 API", tags = {"Auth"})
 @RequiredArgsConstructor
 @RestController
