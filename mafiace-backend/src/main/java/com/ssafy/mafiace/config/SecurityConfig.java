da66f2d2cd544466722d833e7c16340b6f51ac45
조영운 <dnsldnsl07@naver.com>
2022-01-31 20:57:56 +0900

    [S06P12A602-48] fix: URI 수정

--- mafiace-backend/src/main/java/com/ssafy/mafiace/config/SecurityConfig.java
+++ mafiace-backend/src/main/java/com/ssafy/mafiace/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 역시 사용 X
             .and()
             .authorizeRequests() // 요청에 대한 사용권한 체크
-            .antMatchers(HttpMethod.PATCH, "/api/user/update").authenticated()
+            .antMatchers(HttpMethod.PATCH, "/api/user").authenticated()
             .antMatchers("/api/session/*").authenticated()
             .anyRequest().permitAll() // 그외 나머지 요청은 누구나 접근 가능
             .and()
