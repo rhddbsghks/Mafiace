163f75e8565ad500c8ed4d9155a707e37d9a8eaa
gyh8781 <gyh8781@gmail.com>
2022-02-01 01:38:00 +0900

    [S06P12A602-70] feat: 게임 리스트 중간 저장

---
+++ mafiace-backend/src/main/java/com/ssafy/mafiace/api/service/GameService.java
@@ -0,0 +1,10 @@
+package com.ssafy.mafiace.api.service;
+
+import com.ssafy.mafiace.db.entity.Game;
+import java.util.List;
+
+public interface GameService {
+
+    List<Game> getGameList() ;
+    int getGameCount();
+}
