4d3847fae0c486a8cf9036fed8a512b59217802e
gyh8781 <gyh8781@gmail.com>
2022-02-02 03:42:20 +0900

    feat: 기본 방 목록 페이지 생성

--- mafiace-frontend/src/components/main/room/RoomComp.js
+++ mafiace-frontend/src/components/main/room/RoomComp.js
@@ -1,7 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import styles from "./room.module.css";
+import { Icon } from "semantic-ui-react";
 
-const RoomComp = () => {
-  return <div></div>;
+const RoomComp = ({ game }) => {
+  const [randomImg, setRandomImg] = useState([
+    "angry",
+    "happy",
+    "sad",
+    "netural",
+    "panic",
+  ]);
+  const [randomNum, setRandomNum] = useState();
+
+  useEffect(() => {
+    setRandomNum((cir) => Math.floor(Math.random() * 10 + 1) % 5);
+  }, []);
+
+  console.log(game);
+
+  return (
+    <div className={styles.game}>
+      <img
+        src={`./img/${randomImg[randomNum]}.png`}
+        alt=""
+        style={{ height: "60%", margin: "auto" }}
+      />
+
+      <div
+        style={{
+          width: "60%",
+          height: "100%",
+          marginRight: "5.5%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-around",
+        }}
+      >
+        <div className={styles["game-header"]}>
+          <div style={{ margin: "5.5%" }}>
+            {game.roomNum} &nbsp;&nbsp;&nbsp;
+            {game.gameTitle}
+          </div>
+        </div>
+        <div className={styles["game-info"]}>
+          {game.public ? <Icon name="lock" /> : <Icon name="user" />}
+
+          <div style={{ fontSize: "2em" }}>WAITING</div>
+          <div>0/{game.maxPlayer}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RoomComp;
--- mafiace-frontend/src/components/main/room/RoomList.js
+++ mafiace-frontend/src/components/main/room/RoomList.js
@@ -1,24 +1,98 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import RoomComp from "./RoomComp";
+// import RoomComp from "./RoomComp";
 import * as config from "../../../config";
+import styles from "./room.module.css";
+import Loader from "./Loader";
+import RoomComp from "./RoomComp";
 
 const RoomList = ({ getIngame }) => {
   const clickIngame = () => {
     getIngame(true);
   };
 
+  const [loading, setLoading] = useState(true);
   const [list, setList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [maxPlayer, setMaxPlayer] = useState(8);
+  const [isPublic, setIsPublic] = useState(0);
+  const [totalRoom, setTotalRoom] = useState();
+
+  const getGameList = (maxPlayer, isPublic, page) => {
+    axios
+      .get(config.API_URL + "/game", {
+        params: { maxPlayer, isPublic, page },
+      })
+      .then(({ data }) => {
+        setList(data.list);
+        setTotalRoom(data.list.length);
+        setLoading(false);
+      });
+  };
 
   useEffect(() => {
-    axios.get({config.API_URL}+"/asd", {
-      params: { nickname: values.nickname },
-    })
+    getGameList(maxPlayer, isPublic, page);
+    // getGameList(maxPlayer, isPublic, page);
   }, []);
 
   return (
     <>
-      <button onClick={clickIngame}>방 입장</button>
+      {/* <Loader /> 로더 확인해보기*/}
+      {loading ? (
+        <Loader />
+      ) : (
+        <div>
+          {list.length === 0 ? (
+            <div
+              style={{
+                height: "55vh",
+                textAlign: "center",
+              }}
+            >
+              <div className={styles["img-wrap"]}>
+                <img
+                  src={"./img/main.png"}
+                  style={{ height: "20em", marginTop: "9%" }}
+                  alt=""
+                />
+
+                <div
+                  style={{ color: "#8157a8", fontSize: "5em", marginTop: "3%" }}
+                >
+                  진행중인 게임이 없어요.
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                height: "55vh",
+              }}
+            >
+              {list.map((item) => (
+                <RoomComp key={item.id} game={item} />
+              ))}
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row-reverse",
+                  marginTop: "5%",
+                  width: "100%",
+                }}
+              >
+                <button
+                  className={`${styles.button} ${styles["btn-2"]}`}
+                  onClick={clickIngame}
+                >
+                  방 만들기
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
     </>
   );
 };
