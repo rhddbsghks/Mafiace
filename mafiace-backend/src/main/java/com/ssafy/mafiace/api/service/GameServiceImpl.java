6e1729c689dbd555acf77e6b0dc2563d23299685
gyh8781 <gyh8781@gmail.com>
2022-02-02 23:28:24 +0900

    [S06P12A602-70] feat: 게임방 목록 API

--- mafiace-backend/src/main/java/com/ssafy/mafiace/api/service/GameServiceImpl.java
+++ mafiace-backend/src/main/java/com/ssafy/mafiace/api/service/GameServiceImpl.java
@@ -2,6 +2,7 @@ package com.ssafy.mafiace.api.service;
 
 import com.ssafy.mafiace.db.entity.Game;
 import com.ssafy.mafiace.db.repository.GameRepository;
+import com.ssafy.mafiace.db.repository.GameRepositorySupport;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
@@ -10,17 +11,15 @@ public class GameServiceImpl implements GameService {
 
     private GameRepository gameRepository;
 
-    public GameServiceImpl(GameRepository gameRepository) {
+    private GameRepositorySupport gameRepositorySupport;
+
+    public GameServiceImpl(GameRepository gameRepository,GameRepositorySupport gameRepositorySupport) {
         this.gameRepository = gameRepository;
+        this.gameRepositorySupport = gameRepositorySupport;
     }
 
     @Override
-    public List<Game> getGameList() {
-        return gameRepository.findAll();
-    }
-
-    @Override
-    public int getGameCount() {
-        return (int) gameRepository.count();
+    public List<Game> getGameList(int maxPlayer,int isPublic) {
+        return gameRepositorySupport.findGameByOption(maxPlayer, isPublic);
     }
 }
