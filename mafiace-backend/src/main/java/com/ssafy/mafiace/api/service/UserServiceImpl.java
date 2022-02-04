6d7ec961efb2e435fc25f2fe695179513ce6323f
jwkim.ap <jwkim.ap@gmail.com>
2022-02-04 15:02:18 +0900

    [S06P12A602-105] feat: 회원 탈퇴 계정 삭제 처리

--- mafiace-backend/src/main/java/com/ssafy/mafiace/api/service/UserServiceImpl.java
+++ mafiace-backend/src/main/java/com/ssafy/mafiace/api/service/UserServiceImpl.java
@@ -11,12 +11,16 @@ import com.ssafy.mafiace.db.repository.UserRepository;
 import com.ssafy.mafiace.db.repository.UserRepositorySupport;
 import com.ssafy.mafiace.db.repository.DeleteAccountRepository;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@EnableScheduling
 public class UserServiceImpl implements UserService {
 
     @Autowired
@@ -116,7 +120,7 @@ public class UserServiceImpl implements UserService {
     public User deleteAccount(User user) {
         DeleteAccount deleteAccount = DeleteAccount.builder()
             .requestDate(LocalDate.now())
-            .finishDate(LocalDate.now().plusDays(30))
+            .finishDate(LocalDate.now())
             .build();
 
         deleteAccount.setUser(user);
@@ -136,5 +140,17 @@ public class UserServiceImpl implements UserService {
 
         user.restoreAccount(user.getUserId());  // 유저의 탈퇴 여부를 false로 변경
         return userRepository.save(user);
+    }
+
+    // 한국 시간 기준 매일 자정에 탈퇴 신청 후 30일 이상이 지난 회원 정보를 DB에서 삭제
+    @Scheduled(cron = "@midnight", zone = "GMT+9")
+    public void deleteAccountFromServer() {
+        List<DeleteAccount> deleteAccountList = deleteAccountRepository.findAll();
+
+        for (DeleteAccount acc : deleteAccountList) {
+            if (acc.getFinishDate().isBefore(LocalDate.now()) || acc.getFinishDate().equals(LocalDate.now())) {
+                userRepository.deleteById(acc.getId());
+            }
+        }
     }
 }
