b4d645b30b4fc32ccdebe2e2910f172c5f809360
John <john@example.com>
2022-02-04 14:27:51 +0900

    [S06P12A02-44] feat: 헤더 Router useNavigate 적용

--- mafiace-frontend/src/components/main/header/HeaderCompo.js
+++ mafiace-frontend/src/components/main/header/HeaderCompo.js
@@ -1,45 +1,60 @@
 import { Link } from "react-router-dom";
-// import Tabs from "@mui/material/Tabs";
-// import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const HeaderCompo = ({ getLogin }) => {
+  const [value, setValue] = useState(2);
+  let navigate = useNavigate();
+
   const clickLogout = () => {
     localStorage.clear();
     getLogin(false);
   };
 
+  const handleChange = (e, newValue) => {
+    console.log(newValue);
+    setValue(newValue);
+    switch (newValue) {
+      case 0:
+        navigate("/notice");
+        break;
+      case 1:
+        navigate("/rules");
+        break;
+      case 2:
+        navigate("/");
+        break;
+      case 3:
+        navigate("/mypage");
+        break;
+      case 4:
+        navigate("/ranking");
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <>
       <h1>Header</h1>
-      {/* <Tabs centered>
-        <Link to={"/notice"}>
-          <Tab label="공지사항" />
-        </Link>
-        <Link to={"/rules"}>
-          <Tab label="게임방법" />
-        </Link>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="disabled tabs example"
+        centered
+      >
+        <Tab label="공지사항" />
+        <Tab label="게임방법" />
+        <Tab label="방 목록" />
+        <Tab label="내 정보" />
+        <Tab label="명예의 전당" />
         <Link to={"/"}>
-          <Tab label="방 목록" />
+          <button onClick={clickLogout}>로그아웃</button>
         </Link>
-
-        <Link to={"/mypage"}>
-          <Tab label="내 정보" />
-        </Link>
-        <Link to={"/ranking"}>
-          <Tab label="명예의 전당" />
-        </Link>
-      </Tabs> */}
-      <Link to={"/"}>
-        <button onClick={clickLogout}>로그아웃</button>
-      </Link>
-
-      <div>
-        <Link to={"/notice"}>_공지사항_</Link>
-        <Link to={"/rules"}>_게임방법_</Link>
-        <Link to={"/"}>_방 목록_</Link>
-        <Link to={"/mypage"}>_내 정보_</Link>
-        <Link to={"/ranking"}>_명예의 전당_</Link>
-      </div>
+      </Tabs>
     </>
   );
 };
