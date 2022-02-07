0d03d2638190215fa723236aff26d39499485110
조영운 <dnsldnsl07@naver.com>
2022-02-08 01:14:47 +0900

    [S06P12A602-111] feat: 아이디 찾기 기능 추가

--- mafiace-backend/src/main/java/com/ssafy/mafiace/api/controller/UserController.java
+++ mafiace-backend/src/main/java/com/ssafy/mafiace/api/controller/UserController.java
@@ -134,6 +134,22 @@ public class UserController {
         return ResponseEntity.status(200).body(BaseResponseBody.of(200, "사용 가능한 닉네임입니다."));
     }
 
+    @ApiOperation(value = "아이디 찾기", notes = "이메일을 입력받아 아이디의 일부를 알려준다.")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "아이디 찾기가 완료되었습니다."),
+        @ApiResponse(code = 404, message = "해당 계정이 존재하지 않습니다."),
+    })
+    @GetMapping("/id")
+    public ResponseEntity<UserInfoRes> findId(
+        @RequestBody UserRegisterPostReq registerReq) {
+        User user = userService.getUserByEmail(registerReq.getEmail());
+        if (user != null) {
+                return ResponseEntity.status(200).body(UserInfoRes.of(200, "아이디 찾기가 완료되었습니다.",user));
+        }
+        return ResponseEntity.status(404)
+            .body(UserInfoRes.of(404, "해당 계정이 존재하지 않습니다.", null));
+    }
+
     @ApiOperation(value = "비밀번호 찾기", notes = "아이디와 이메일을 입력받아서 임시 비밀번호를 해당 이메일로 전송한다.")
     @ApiResponses({
         @ApiResponse(code = 200, message = "메일 전송이 완료되었습니다."),
