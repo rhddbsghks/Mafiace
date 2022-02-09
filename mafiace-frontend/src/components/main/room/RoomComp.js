62e085822be141d1cd820de32eab1e0d88603389
gyh8781 <gyh8781@gmail.com>
2022-02-10 01:48:27 +0900

    [S06P12A602-135] feat: 방 비밀번호 체크

--- mafiace-frontend/src/components/main/room/RoomComp.js
+++ mafiace-frontend/src/components/main/room/RoomComp.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import styles from "./room.module.css";
-import { Icon } from "semantic-ui-react";
+import { Icon, Modal, Form } from "semantic-ui-react";
 
 const RoomComp = ({
   game,
@@ -13,6 +13,8 @@ const RoomComp = ({
 }) => {
   const randomImg = useState(["angry", "happy", "sad", "netural", "panic"])[0];
   const [randomNum, setRandomNum] = useState();
+  const [openPasswordModal, setOpenPasswordModal] = useState(false);
+  const [password, setPassword] = useState("");
 
   useEffect(() => {
     setRandomNum((cur) => Math.floor(Math.random() * 10 + 1) % 5);
@@ -28,6 +30,23 @@ const RoomComp = ({
   };
 
   const enterRoom = () => {
+    if (!game.public) {
+      if (!openPasswordModal) {
+        setOpenPasswordModal(true);
+        return;
+      } else {
+        axios
+          .get("/mafiace/api/game/checkpw", {
+            headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` },
+            params: { sessionName: game.id, password },
+          })
+          .catch(() => {
+            alert("비밀번호가 일치하지 않아요!");
+            return;
+          });
+      }
+    }
+
     axios
       .get("/mafiace/api/session/token", {
         headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` },
@@ -55,47 +74,108 @@ const RoomComp = ({
   };
 
   return (
-    <div className={styles.game} onClick={handleClickRoom}>
-      <img
-        src={`/img/${randomImg[randomNum]}.png`}
-        alt=""
-        style={{ height: "60%", margin: "auto" }}
-      />
-
-      <div
-        style={{
-          width: "60%",
-          height: "100%",
-          marginRight: "5.5%",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-around",
-        }}
+    <>
+      <Modal
+        dimmer="inverted"
+        size="tiny"
+        open={openPasswordModal}
+        onClose={() => setOpenPasswordModal(false)}
+        onOpen={() => setOpenPasswordModal(true)}
+        className="make-body"
       >
-        <div className={styles["game-header"]}>
-          <div style={{ margin: "5.5%" }}>
-            <span style={{ fontSize: "0.7em" }}>
-              {game.roomNum}&nbsp;&nbsp;&nbsp;&nbsp;
-            </span>
-            <span style={{ fontSize: "34px" }}>{game.gameTitle}</span>
-          </div>
-        </div>
-        <div className={styles["game-info"]}>
-          {game.public ? <Icon name="user" /> : <Icon name="lock" />}
+        <div className="room-make-box" style={{ height: "100%" }}>
+          <Form unstackable style={{ height: "100%" }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                height: "100%",
+              }}
+            >
+              <div className="room-make-box-title">비밀번호 입력</div>
+              <div>
+                <Form.Field>
+                  <input
+                    onChange={({ target }) => {
+                      setPassword(target.value);
+                    }}
+                    placeholder="비밀번호"
+                    type="password"
+                    required
+                  />
+                </Form.Field>
+              </div>
 
-          <div style={{ fontSize: "2em" }}>
-            {game.maxPlayer % 2 === 0 ? (
-              <span style={{ color: "#7f7c824d" }}>PLAYING</span>
-            ) : (
-              <span style={{ color: "#8157a8" }}>WAITING</span>
-            )}
+              <div
+                style={{
+                  display: "flex",
+                  height: "50px",
+                  justifyContent: "center",
+                  marginTop: "10%",
+                }}
+              >
+                <button
+                  className="create-room-btn make"
+                  onClick={() => {
+                    enterRoom();
+                  }}
+                >
+                  확인
+                </button>
+                <button
+                  className="create-room-btn cancel"
+                  onClick={() => setOpenPasswordModal(false)}
+                >
+                  취소
+                </button>
+              </div>
+            </div>
+          </Form>
+        </div>
+      </Modal>
+      <div className={styles.game} onClick={handleClickRoom}>
+        <img
+          src={`/img/${randomImg[randomNum]}.png`}
+          alt=""
+          style={{ height: "60%", margin: "auto" }}
+        />
+
+        <div
+          style={{
+            width: "60%",
+            height: "100%",
+            marginRight: "5.5%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-around",
+          }}
+        >
+          <div className={styles["game-header"]}>
+            <div style={{ margin: "5.5%" }}>
+              <span style={{ fontSize: "0.7em" }}>
+                {game.roomNum}&nbsp;&nbsp;&nbsp;&nbsp;
+              </span>
+              <span style={{ fontSize: "34px" }}>{game.gameTitle}</span>
+            </div>
           </div>
-          <div>
-            {participantCount}/{game.maxPlayer}
+          <div className={styles["game-info"]}>
+            {game.public ? <Icon name="user" /> : <Icon name="lock" />}
+
+            <div style={{ fontSize: "2em" }}>
+              {game.maxPlayer % 2 === 0 ? (
+                <span style={{ color: "#7f7c824d" }}>PLAYING</span>
+              ) : (
+                <span style={{ color: "#8157a8" }}>WAITING</span>
+              )}
+            </div>
+            <div>
+              {participantCount}/{game.maxPlayer}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
