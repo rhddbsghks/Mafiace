6635329fb730b64d0efae8e39c2865530dfea092
gyh8781 <gyh8781@gmail.com>
2022-02-11 03:44:29 +0900

    [S06P12A602-138] feat: 방장 표식

--- mafiace-frontend/src/components/ingame/ingame/UserVideoComponent.js
+++ mafiace-frontend/src/components/ingame/ingame/UserVideoComponent.js
@@ -1,15 +1,42 @@
 import React from "react";
+import { useEffect } from "react";
 import OpenViduVideoComponent from "./OvVideo";
 import "./UserVideo.css";
 
-const UserVideoComponent = ({ streamManager }) => {
+const UserVideoComponent = ({ streamManager, ownerId }) => {
   const nickNameTag = JSON.parse(streamManager.stream.connection.data).nickName;
+  const id = JSON.parse(streamManager.stream.connection.data).id;
 
   return (
     <div>
       {streamManager !== undefined ? (
         <div className="streamcomponent">
+          {ownerId === id ? (
+            <img
+              src={`/img/crown.png`}
+              alt=""
+              style={{
+                width: "35px",
+                position: "relative",
+                top: "10px",
+                left: "-5px",
+              }}
+            />
+          ) : (
+            <img
+              src={`/img/crown.png`}
+              alt=""
+              style={{
+                visibility: "hidden",
+                width: "35px",
+                position: "relative",
+                top: "10px",
+                left: "-5px",
+              }}
+            />
+          )}
           <OpenViduVideoComponent streamManager={streamManager} />
+
           <div style={{ width: "11%", height: "5.5%", display: "flex" }}>
             <span
               style={{
