a8c30ae2bb6ffa18276f610a899588777c1e516d
연진우 <2dwd10@gmail.com>
2022-02-10 10:01:37 +0900

    feat: face-api Obejct값 array로

--- mafiace-frontend/src/components/ingame/ingame/OvVideo.js
+++ mafiace-frontend/src/components/ingame/ingame/OvVideo.js
@@ -5,7 +5,18 @@ import { useState } from "react";
 
 const OpenViduVideoComponen = ({ streamManager }) => {
   const videoRef = useRef();
-  const [faceExpressions, setFaceExpressions] = useState({});
+  const [faceExpressions, setFaceExpressions] = useState({
+    neutral: 0,
+    happy: 0,
+    sad: 0,
+    angry: 0,
+    fearful: 0,
+    disgusted: 0,
+    surprised: 0,
+  });
+  const [chk, setChk] = useState(false);
+  const [arrExpressions, setArrExpressions] = useState([]);
+  const [topEmotion, setTopEmotion] = useState("");
 
   const onPlay = async () => {
     if (
@@ -23,13 +34,15 @@ const OpenViduVideoComponen = ({ streamManager }) => {
         .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
         .withFaceLandmarks()
         .withFaceExpressions();
+      // console.log(streamManager.stream.connection.connectionId + "의 감정정보");
 
-      console.log(streamManager.stream.connection.connectionId + "의 감정정보");
-
-      if (!detections[0]) console.log("감지안됨");
-      else {
-        console.log(detections[0].expressions);
+      if (!detections[0]) {
+        setChk(false);
+      } else {
         setFaceExpressions(detections[0].expressions);
+        setArrExpressions(Object.entries(faceExpressions));
+        // console.log(detections[0].expressions);
+        setChk(true);
       }
     }, 1000);
   };
@@ -49,9 +62,23 @@ const OpenViduVideoComponen = ({ streamManager }) => {
     }
   }, []);
 
+  const clickBtn = () => {
+    // console.log(faceExpressions);
+    if (chk) {
+      console.log(arrExpressions);
+      // const valSort = arrExpressions.sort(([, a], [, b]) => b - a);
+      // setTopEmotion(valSort[0][0]);
+      // console.log(valSort);
+      // console.log(topEmotion);
+    } else {
+      console.log("face is not detectied");
+    }
+  };
+
   return (
     <>
       <video autoPlay={true} ref={videoRef} onPlaying={onPlay} />
+      <button onClick={clickBtn}>button</button>
     </>
   );
 };
