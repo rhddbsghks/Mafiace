2e028026f73e00837af1d85d7be71d6e3de1cba4
jwkim.ap <jwkim.ap@gmail.com>
2022-02-10 18:15:39 +0900

    [S06P12A602-128] feat: 투표 로직 작성 - 게임 결과 반환

--- mafiace-backend/src/main/java/com/ssafy/mafiace/db/manager/VoteManager.java
+++ mafiace-backend/src/main/java/com/ssafy/mafiace/db/manager/VoteManager.java
@@ -3,6 +3,7 @@ package com.ssafy.mafiace.db.manager;
 import com.ssafy.mafiace.game.Player;
 import com.ssafy.mafiace.api.response.GamePlayerRes;
 import com.ssafy.mafiace.api.response.GameResult;
+import com.ssafy.mafiace.api.response.GameResultType;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,4 +18,15 @@ public class VoteManager {
         this.voteStatus = new HashMap<>(this.gamePlayerRes.getPlayersNumber());
     }
 
+    // 게임 결과 반환
+    public GameResult getGameResult () {
+        GameResultType gameResultType = this.gamePlayerRes.confirmGameStatus();
+        switch (gameResultType) {
+            case MafiaWin:
+                return GameResult.returnMafiaWin();
+            case CitizenWin:
+                return GameResult.returnCitizenWin();
+            // 게임 계속 진행하는 로직 필요
+        }
+    }
 }
