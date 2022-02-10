59a176942cf7878886129dc50979ce986ad292b4
gyh8781 <gyh8781@gmail.com>
2022-02-10 17:09:37 +0900

    feat: 인게임 화면 수정

--- mafiace-frontend/src/components/ingame/headerIngame/HeaderIngameCompo.js
+++ mafiace-frontend/src/components/ingame/headerIngame/HeaderIngameCompo.js
@@ -1,7 +1,9 @@
 const HeaderIngameCompo = () => {
   return (
     <>
-      <h1>HeaderIngame__Compo</h1>
+      <h1 style={{ margin: 0, position: "absolute", zIndex: "0" }}>
+        HeaderIngame__Compo
+      </h1>
     </>
   );
 };
