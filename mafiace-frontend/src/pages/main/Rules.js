581b062374c2774049ef92f7af8377dd38253433
gyh8781 <gyh8781@gmail.com>
2022-02-16 12:21:04 +0900

    conflict 해결

--- mafiace-frontend/src/pages/main/Rules.js
+++ mafiace-frontend/src/pages/main/Rules.js
@@ -3,11 +3,6 @@ import RulesCompo from "../../components/main/rules/RulesCompo";
 const Rules = () => {
   return (
     <>
-<<<<<<< HEAD
-      <h1 style={{ textAlign: "center", fontSize: "300%" }}>Rules</h1>
-=======
-      {/* <h1 style={{ textAlign: "center", fontSize: "300%" }}>Rules</h1> */}
->>>>>>> 72ff2927f67baf85d940564a5fd5d2690f6f1598
       <RulesCompo />
     </>
   );
