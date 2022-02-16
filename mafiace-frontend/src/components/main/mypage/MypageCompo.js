d8e15f3b0b232f741624544b5e6fd7865b2a507c
gyh8781 <gyh8781@gmail.com>
2022-02-16 16:03:42 +0900

    feat: 기록실 생성 및 UI 수정

--- mafiace-frontend/src/components/main/mypage/MypageCompo.js
+++ mafiace-frontend/src/components/main/mypage/MypageCompo.js
@@ -2,46 +2,19 @@ import React, { useState, useEffect, useRef } from "react";
 import {
   Button,
   Divider,
-  Header,
   Icon,
-  Table,
   Container,
-  Statistic,
   Image,
   Form,
   Modal,
 } from "semantic-ui-react";
 import axios from "axios";
 import Loader from "../../common/Loader";
-import Honor from "./Honor";
 import jwt from "jwt-decode";
 
 const MypageCompo = () => {
-  const korJobName = {
-    Police: "경찰",
-    Doctor: "의사",
-    Citizen: "시민",
-    Mafia: "마피아",
-  };
-  const honors = [
-    "citizen3Play",
-    "police3Play",
-    "doctor3Play",
-    "mafia3Play",
-    "citizen10Play",
-    "police10Play",
-    "doctor10Play",
-    "mafia10Play",
-    "firstWin",
-    "investigate10",
-    "save10",
-    "kill10",
-  ];
   const [form, setForm] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [winlose, setWinlose] = useState([]);
-  const [latestLog, setLatestlog] = useState([]);
-  const [myHonors, setMyHonors] = useState(new Set());
   const [passwordModal, setPasswordModal] = useState(false);
   const [nicknameModal, setNicknameModal] = useState(false);
   const [deleteModal, setDeleteModal] = useState(false);
@@ -73,33 +46,6 @@ const MypageCompo = () => {
         }
       });
     // console.log(form);
-
-    axios
-      .post(
-        "/mafiace/api/user/userRecord",
-        {},
-        {
-          headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` },
-        }
-      )
-      .then(({ data }) => {
-        console.log(data);
-        setMyHonors(new Set(data.honors));
-        setLatestlog(data.records);
-        setWinlose(data.userRecords);
-      })
-      .catch(({ response }) => {
-        console.log(response);
-        if (
-          response.status === 500 ||
-          response.status === 401 ||
-          response.status === 403
-        ) {
-          alert("만료된 토큰입니다.");
-          localStorage.removeItem("jwt");
-          window.location.href = "/";
-        }
-      });
   }, []);
 
   const modifyPassword = () => {
@@ -196,12 +142,12 @@ const MypageCompo = () => {
         <Loader msg="로딩 중..." />
       ) : (
         <>
-          <Divider horizontal>
+          {/* <Divider horizontal>
             <div style={{ fontSize: "3em" }}>
               <Icon name="tag" style={{ fontSize: "0.5em" }} />
               회원 정보
             </div>
-          </Divider>
+          </Divider> */}
           <Image src="" size="small" />
           <div
             style={{
@@ -558,160 +504,6 @@ const MypageCompo = () => {
               </div>
             </Modal>
           </div>
-
-          <Divider horizontal style={{ marginTop: "10%" }}>
-            <div style={{ fontSize: "3em" }}>
-              <Icon name="bar chart" style={{ fontSize: "0.5em" }} />
-              전적
-            </div>
-          </Divider>
-
-          <div style={{ margin: "auto" }}>
-            <Statistic.Group style={{ justifyContent: "center" }}>
-              <Statistic>
-                <Statistic.Value style={{ fontSize: "6em !important" }}>
-                  {winlose.winCount}
-                </Statistic.Value>
-                <Statistic.Label
-                  style={{
-                    color: "#b465fdd8",
-                    fontSize: "30px",
-                    marginTop: "10%",
-                  }}
-                >
-                  Win
-                </Statistic.Label>
-              </Statistic>
-              <Statistic>
-                <Statistic.Value style={{ fontSize: "6em !important" }}>
-                  {winlose.loseCount}
-                </Statistic.Value>
-                <Statistic.Label
-                  style={{
-                    color: "#d36d97d8",
-                    fontSize: "30px",
-                    marginTop: "10%",
-                  }}
-                >
-                  Lose
-                </Statistic.Label>
-              </Statistic>
-              <Statistic>
-                <Statistic.Value style={{ fontSize: "6em !important" }}>
-                  {winlose.loseCount + winlose.winCount}
-                </Statistic.Value>
-                <Statistic.Label
-                  style={{
-                    color: "#7f7c8293",
-                    fontSize: "30px",
-                    marginTop: "10%",
-                  }}
-                >
-                  Total
-                </Statistic.Label>
-              </Statistic>
-            </Statistic.Group>
-
-            <div style={{ textAlign: "center", marginTop: "5%" }}>
-              <Icon name="chart line" />
-              <span style={{ fontSize: "2em" }}> 최근 10게임</span>
-            </div>
-
-            <Table>
-              <thead
-                style={{ textAlign: "center", backgroundColor: "purple-200" }}
-              >
-                <tr>
-                  <th style={{ fontSize: "2rem" }}>Job</th>
-                  <th style={{ fontSize: "2rem" }}>Result</th>
-                  <th style={{ fontSize: "2rem" }}>Play Time</th>
-                </tr>
-              </thead>
-              <tbody>
-                {latestLog.map((item) => {
-                  return (
-                    <tr
-                      className="bg-white border-2 border-gray-200"
-                      style={{
-                        textAlign: "center",
-                        borderBottom: "1px dotted gray",
-                      }}
-                    >
-                      <td
-                        className="px-4 py-3"
-                        style={{
-                          textAlign: "center",
-                        }}
-                      >
-                        <div
-                          style={{
-                            width: "100px",
-                            height: "50px",
-                            margin: "1% auto",
-                          }}
-                        >
-                          <img
-                            src={`img/${item.role}.png`}
-                            alt=""
-                            height="100%"
-                          />
-                        </div>
-
-                        <span
-                          className="hover:text-blue-500 cursor-pointer"
-                          style={{ fontSize: "2em" }}
-                        >
-                          {korJobName[item.role]}
-                        </span>
-                      </td>
-                      <td className="px-4 py-3">
-                        {item.win === true ? (
-                          <p
-                            className="hover:text-blue-500 cursor-pointer"
-                            style={{ fontSize: "4em", color: "#b465fdd8" }}
-                          >
-                            Win
-                          </p>
-                        ) : (
-                          <p
-                            className="hover:text-blue-500 cursor-pointer"
-                            style={{ fontSize: "4em", color: "#d36d97d8" }}
-                          >
-                            Lose
-                          </p>
-                        )}
-                      </td>
-                      <td className="px-4 py-3" style={{ fontSize: "3em" }}>
-                        {item.playTime}
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </Table>
-          </div>
-          <Divider horizontal style={{ marginTop: "10%" }}>
-            <div style={{ fontSize: "3em" }}>
-              <Icon name="trophy" style={{ fontSize: "0.5em" }} />
-              업적
-            </div>
-          </Divider>
-          <section
-            style={{
-              width: "90%",
-              height: "700px",
-              position: "relative",
-              margin: "5% auto",
-              display: "flex",
-              justifyContent: "space-between",
-              alignContent: "space-between",
-              flexWrap: "wrap",
-            }}
-          >
-            {honors.map((item) => (
-              <Honor honorName={item} get={myHonors.has(item)} />
-            ))}
-          </section>
         </>
       )}
     </div>
